e85d238c0a205ba36fe42494728a4f669d6f0191
jrkarnes <jrkn87@mail.umkc.edu>
2017-02-02 17:27:27 -0600

    Addended the metadata and changelog: -- Chef Supermarket changes for issues_url, source_url, and supported platforms

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -10,3 +10,9 @@ This file is used to list changes made in each version of the azure_fqdn_fix coo
 
 ## 1.0.1
 - [Justin Karnes] - Addendum to readme to support supermarket markdown
+
+## 1.0.2
+- [Justin Karnes] - Update Metadata to include source URL and issues URL
+
+## 1.0.3
+- [Justin Karnes] - Update Metadata to include supported platforms
--- metadata.rb
+++ metadata.rb
@@ -4,5 +4,9 @@ maintainer_email 'justin@perfectcube.co'
 license          'MIT'
 description      'Fixes node FQDNs on Azure without hassle'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.0.1'
+version          '1.0.3'
 depends "ohai"
+source_url "https://github.com/jrkarnes/azure_fqdn_fix"
+issues_url "https://github.com/jrkarnes/azure_fqdn_fix/issues"
+
+supports 'ubuntu', '>= 14.04'
